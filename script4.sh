6554383d9a9bb8cbffe9a55eb3f936a14b4f9fe4
JanLaveau <159476861+JanLaveau@users.noreply.github.com>
2024-02-08 13:10:02 -0600

    Add files via upload

---
+++ script4.sh
@@ -0,0 +1,12 @@
+#scrip3 - uso de variables
+#8/02/24
+
+#creando variables
+directorio1=ex1
+directorio2=ex2
+directorio3=ex3
+rais=practica3
+
+echo generando directorios
+mkdir -p $rais/$directorio1/fx1/fx2
+echo generado con exito.
