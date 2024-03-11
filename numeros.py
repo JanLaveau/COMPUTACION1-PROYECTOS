234929612fcd088f7e40af8061894afce69d500a
JanLaveau <159476861+JanLaveau@users.noreply.github.com>
2024-03-11 14:00:52 -0600

    Create numeros.py

---
+++ numeros.py
@@ -0,0 +1,20 @@
+print("introduce a")
+a=int(input())
+print("introduce b")
+b=int(input())
+
+if a==b:
+    print("true")
+
+elif a!=b:
+    print("false")
+elif a>b: 
+    print("true")
+else:
+    print("false")
+
+elif a!=b AND a>b:
+    print(a>b)
+elif a!=b AND a<b:
+    print(a<b)
+
